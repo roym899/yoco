b55c7253bc4c29cc6872af86df97a0009ed29ff7
Leonard Bruns <roym899@gmail.com>
2021-02-25 18:45:14 +0100

    Add test for nested and multiple parent files

--- tests/test_yoco.py
+++ tests/test_yoco.py
@@ -49,3 +49,31 @@ def test_save_config(tmp_path):
     yoco.save_config_to_file(file_path, original_dict)
     new_dict = yoco.load_config_from_file(file_path)
     assert original_dict == new_dict
+
+
+def test_nested_config(tmp_path):
+    """Test loading with multiple nested config files.
+
+    Loads a config file that contains two parent configs.
+    One of which has another parent:
+
+    start -> 1
+            / \
+           2   3
+           |
+           4 <- "parent" of 2
+
+    The higher up in the chain, the higher the priority.
+    If there are multiple parents, the last one wins.
+    """
+    original_dict = yoco.load_config_from_file("tests/test_files/1.yaml")
+    expected_dict = {
+        "2_and_4": 2,  # 2.yaml is higher up in the chain
+        "4_only": 4,
+        "2_and_3": 3,  # 3.yaml is the the last parent of 1
+        "3_only": 3,
+        "1_only": 1,
+        "2_only": 2,
+        "all": 1,
+    }
+    assert original_dict == expected_dict
