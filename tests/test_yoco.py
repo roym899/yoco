1d08639e61d0b9fe4f21b17bba6700c90c8be00d
Leonard Bruns <roym899@gmail.com>
2021-02-25 18:07:49 +0100

    Add test to load config from dictionary

--- tests/test_yoco.py
+++ tests/test_yoco.py
@@ -9,7 +9,30 @@ def test_config_from_file():
     expected_dict = {
         "test_param_1": 2,
         "test_param_2": "Test string",
-        "test_list": [1, 2, 3]
+        "test_list": [1, 2, 3],
     }
 
     assert loaded_dict == expected_dict
+
+
+def test_load_config():
+    """Test loading a config through a predefined config dictionary.
+
+    YOCO should interpret "config" key as a path and follow such paths recursively.
+    When parameters are repeated the one earlier in this chain "wins".
+    """
+    config_dict = {
+        "config": "tests/test_files/config_1.yaml",
+        "test_param_1": 3,  # final param should be 3, not 2 from the file
+        "test_param_3": "Param not in file",
+    }
+    config_dict = yoco.load_config(config_dict)
+
+    # Merged dictionary from config_1.yaml and the provided params
+    expected_dict = {
+        "test_param_1": 3,
+        "test_param_2": "Test string",
+        "test_list": [1, 2, 3],
+        "test_param_3": "Param not in file",
+    }
+    assert config_dict == expected_dict
