d905981352c2e9103109572f8df7bb3717958b0f
Leonard Bruns <leonardb@kth.se>
2022-03-29 15:23:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yoco",
-    version="1.0.2",
+    version="1.1.0",
     author="Leonard Bruns",
     author_email="roym899@gmail.com",
     description="Minimalistic YAML-based configuration system",
