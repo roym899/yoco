dd9a2304679e363c60c83e26736b0f65aa9f2b92
Leonard Bruns <roym899@gmail.com>
2026-02-02 12:20:07 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yoco",
-    version="1.2.1",
+    version="1.3.0",
     author="Leonard Bruns",
     author_email="roym899@gmail.com",
     description="Minimalistic YAML-based configuration system",
